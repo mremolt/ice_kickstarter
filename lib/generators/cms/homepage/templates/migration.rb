5d4982f0d4515f884c80b9f375c5466d8812ba4e
Falk Köppe <falk.koeppe@infopark.de>
2013-01-25 17:22:35 +0100

    removes not yet working homepage generator

--- lib/generators/cms/homepage/templates/migration.rb
+++
@@ -1,5 +0,0 @@
-class CreateStructure < ::RailsConnector::Migration
-  def up
-    create_obj(:_path => '/website/<%= file_name %>', :_obj_class => 'Homepage', :title => '<%= title %>', :locale => '<%= file_name %>')
-  end
-end
