314b7d247ce6dfcaafdaca84176379a32e3b5768
Falk Köppe <falk.koeppe@infopark.de>
2013-02-13 13:34:54 +0100

    bugfix: assets are already precompiled, shared assets should only be prepended to the "before_migrate.rb" file

--- lib/generators/cms/kickstart/kickstart_generator.rb
+++ lib/generators/cms/kickstart/kickstart_generator.rb
@@ -78,7 +78,7 @@ module Cms
       def create_deploy_hooks
         create_file('deploy/after_restart.rb')
         create_file('deploy/before_symlink.rb')
-        create_file('deploy/before_migrate.rb')
+        prepend_file('deploy/before_migrate', 'deploy/before_migrate.rb')
         empty_directory('deploy/templates')
       end
 
